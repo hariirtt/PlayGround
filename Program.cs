451004abe8f73d1f5eff48af1571268efaf2ecad
Harihara Sudhan <hari@hbiz.in>
2015-07-23 11:27:57 +0530

    First file to be added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AVLTree
+{
+
+    class Node
+    {
+        int value;
+        int height;
+        Node left = null;
+        Node right = null;
+        
+        Node(int i)
+        {
+            value = i;
+        }
+
+    };
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
